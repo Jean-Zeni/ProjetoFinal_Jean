1747727dc330ce8b46d5e36be778accb49b00bde
Jean Zeni <pereirazenij@gmail.com>
2024-12-13 23:30:42 -0300

    Commit de 13/12/2024 às 23h30

--- php/home.php
+++ php/home.php
@@ -26,28 +26,34 @@ $usu = new Usuario($db);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 
 <body id="telaHome">
 
     <header>HOME</header><br>
 
-    <button id="btn1" onclick="location.href='cadLivro.php'">Cadastrar Novo Livro/Produto</button><br><br>
+    <div id="botoesHome">
 
-    <button id="btn2" onclick="location.href='listaLivros.php'">Livros</button><br><br>
+    <button class="btnHome" onclick="location.href='cadLivro.php'">Cadastrar Novo Livro/Produto</button><br><br>
 
-    <button id="btn3" onclick="location.href='cadAutor.php'">Cadastrar Novo Autor</button><br><br>
+    <button class="btnHome" onclick="location.href='listaLivros.php'">Livros</button><br><br>
 
-    <button id="btn4" onclick="location.href='listaAutores.php'">Autores Cadastrados</button><br><br>
+    <button class="btnHome" onclick="location.href='cadAutor.php'">Cadastrar Novo Autor</button><br><br>
 
-    <button id="btn5" onclick="location.href='cadEditora.php'">Cadastrar Nova Editora</button><br><br>
+    <button class="btnHome" onclick="location.href='listaAutores.php'">Autores Cadastrados</button><br><br>
 
-    <button id="btn6" onclick="location.href='listaEditoras.php'">Editoras Cadastradas</button><br><br>
+    <button class="btnHome" onclick="location.href='cadEditora.php'">Cadastrar Nova Editora</button><br><br>
 
-    
+    <button class="btnHome" onclick="location.href='listaEditoras.php'">Editoras Cadastradas</button><br><br>
 
     <button id="btnLogout" onclick="location.href='logout.php'">Fazer logout</button><br><br>
+
+    </div>
+
+    <footer>
+        <p>Desenvolvido por Jean Pereira Zeni</p>
+    </footer>
 
 </body>
 
