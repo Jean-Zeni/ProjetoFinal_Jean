7a4ef73139fc4f9ddd019d8e7b650c4663ade307
Jean Zeni <149632193+Jean-Zeni@users.noreply.github.com>
2024-12-18 17:37:40 -0300

    Commit database

--- bd_livraria.sql
+++ bd_livraria.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17-Dez-2024 às 23:08
+-- Tempo de geração: 18/12/2024 às 21:36
 -- Versão do servidor: 10.4.32-MariaDB
--- versão do PHP: 8.0.30
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,7 +26,7 @@ USE `bd_livraria`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_autores`
+-- Estrutura para tabela `tb_autores`
 --
 
 CREATE TABLE `tb_autores` (
@@ -36,19 +36,21 @@ CREATE TABLE `tb_autores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `tb_autores`
+-- Despejando dados para a tabela `tb_autores`
 --
 
 INSERT INTO `tb_autores` (`pk_id_autor`, `nome_autor`, `nacionalidade_autor`) VALUES
-(1, 'Clive Staples Lewis', 'Britânico(Irlandês)'),
-(4, 'Tolkien', 'Britânico'),
+(1, 'C.S. Lewis', 'Britânico(Irlandês)'),
+(4, 'J.R.R. Tolkien', 'Britânico'),
 (6, 'George Orwell', 'Britânico (Índia colonial)'),
-(10, 'Machado de Assis', 'Brasileiro');
+(10, 'Machado de Assis', 'Brasileiro'),
+(13, 'Augusto Cury', 'Brasileiro'),
+(14, 'Victor Hugo', 'Francês');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_editoras`
+-- Estrutura para tabela `tb_editoras`
 --
 
 CREATE TABLE `tb_editoras` (
@@ -58,17 +60,18 @@ CREATE TABLE `tb_editoras` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `tb_editoras`
+-- Despejando dados para a tabela `tb_editoras`
 --
 
 INSERT INTO `tb_editoras` (`pk_id_editora`, `nome_editora`, `infos_editora`) VALUES
 (2, 'Thomas Nelson', 'Veja nossas publicações dos livros de Clive Staples Lewis!'),
-(3, 'Companhia das Letras', 'Veja nossas publicações de George Orwell\r\n');
+(3, 'Companhia das Letras', 'Veja nossas publicações de George Orwell\r\n'),
+(5, 'Vitrola', 'Publicamos seus títulos favoritos');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_livros`
+-- Estrutura para tabela `tb_livros`
 --
 
 CREATE TABLE `tb_livros` (
@@ -83,21 +86,17 @@ CREATE TABLE `tb_livros` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `tb_livros`
+-- Despejando dados para a tabela `tb_livros`
 --
 
 INSERT INTO `tb_livros` (`pk_id_livro`, `nome_livro`, `data_publicacao_livro`, `valor_livro`, `img_livro`, `unidades`, `fk_id_autor`, `fk_id_editora`) VALUES
-(17, 'Cristianismo Puro e Simples ', '1952-11-11', 44.9, '../uploads/6760c7d3f3c93.jpg', 17, 1, 2),
-(18, 'A Revolução dos Bichos', '1967-11-11', 11.9, '../uploads/675dd3105f821.jpeg', 0, 6, 3),
-(19, 'O Pequeno Príncipe', '1976-11-11', 11.9, '../uploads/675deae087e02.jpg', 0, 4, 2),
-(21, 'Dom Casmurro ', '1860-11-11', 44.9, '../uploads/6760c6bfc6d7a.jpg', 19, 10, 3),
-(22, 'Memórias Póstumas de Brás Cubas', '1879-11-11', 22.9, '../uploads/6760c50fa5be1.jpg', 30, 10, 3),
-(23, 'O Espelho e Outros Contos', '1876-11-11', 55.99, '../uploads/6760c54d05aa1.jpg', 5, 10, 3);
+(18, 'A Revolução dos Bichos    ', '1967-11-11', 11.9, '../uploads/67631cadbeb67.jpeg', 5, 6, 3),
+(24, 'Os Miseráveis', '1878-11-11', 65.9, '../uploads/67631c8c38ea8.jpeg', 3, 14, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tb_usuario`
+-- Estrutura para tabela `tb_usuario`
 --
 
 CREATE TABLE `tb_usuario` (
@@ -108,33 +107,33 @@ CREATE TABLE `tb_usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `tb_usuario`
+-- Despejando dados para a tabela `tb_usuario`
 --
 
 INSERT INTO `tb_usuario` (`pk_id_usuario`, `nome_usuario`, `email_usuario`, `senha_usuario`) VALUES
-(9, 'Maria Joana', 'jojo@gmail.com', '$2y$10$/j50AY0L3i0PHpoVsNkt/uEDZmJKouZQFzwbeueLlelx9cxQoDBua'),
-(10, 'Maria Zeni', 'mae@gmail.com', '$2y$10$pWnofbQaT3s3qv70KfS3GuinSJBe.lzUN6bOy7VzMHRxD9goljq.y'),
-(11, 'Jean', 'jean@gmail.com', '$2y$10$G9ntLqplOHKLdiYEqb9dzeNbjog6zoV75w5Esc.wHwjP3M18ioEyO'),
-(12, 'Ana Paula', 'ana@gmail.com', '$2y$10$Es.lTKue99DrRGBJkViomeXxnCvpMxEsuPkJ2GJrhH30reMkDhOBC');
+(14, 'Jean', 'jean@gmail.com', '$2y$10$ZM7GgspDny/QWYqCUmUKIeMPwEC6IPG/55Q.T/ebDszp6VMCWvo8y'),
+(15, 'Cleberson', 'Clebito@gmail.com', '$2y$10$Tmtj.3CeTuD7F7UwmiZz3OP3iiLshoqYxpnvA1MYDJA.IgIrVQxGS');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `tb_autores`
+-- Índices de tabela `tb_autores`
 --
 ALTER TABLE `tb_autores`
-  ADD PRIMARY KEY (`pk_id_autor`);
+  ADD PRIMARY KEY (`pk_id_autor`),
+  ADD UNIQUE KEY `nome_autor` (`nome_autor`);
 
 --
--- Índices para tabela `tb_editoras`
+-- Índices de tabela `tb_editoras`
 --
 ALTER TABLE `tb_editoras`
-  ADD PRIMARY KEY (`pk_id_editora`);
+  ADD PRIMARY KEY (`pk_id_editora`),
+  ADD UNIQUE KEY `nome_editora` (`nome_editora`);
 
 --
--- Índices para tabela `tb_livros`
+-- Índices de tabela `tb_livros`
 --
 ALTER TABLE `tb_livros`
   ADD PRIMARY KEY (`pk_id_livro`),
@@ -142,45 +141,46 @@ ALTER TABLE `tb_livros`
   ADD KEY `fk_id_editora` (`fk_id_editora`);
 
 --
--- Índices para tabela `tb_usuario`
+-- Índices de tabela `tb_usuario`
 --
 ALTER TABLE `tb_usuario`
-  ADD PRIMARY KEY (`pk_id_usuario`);
+  ADD PRIMARY KEY (`pk_id_usuario`),
+  ADD UNIQUE KEY `email_usuario` (`email_usuario`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `tb_autores`
 --
 ALTER TABLE `tb_autores`
-  MODIFY `pk_id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `pk_id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de tabela `tb_editoras`
 --
 ALTER TABLE `tb_editoras`
-  MODIFY `pk_id_editora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `pk_id_editora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `tb_livros`
 --
 ALTER TABLE `tb_livros`
-  MODIFY `pk_id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `pk_id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de tabela `tb_usuario`
 --
 ALTER TABLE `tb_usuario`
-  MODIFY `pk_id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `pk_id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `tb_livros`
+-- Restrições para tabelas `tb_livros`
 --
 ALTER TABLE `tb_livros`
   ADD CONSTRAINT `tb_livros_ibfk_1` FOREIGN KEY (`fk_id_autor`) REFERENCES `tb_autores` (`pk_id_autor`),
